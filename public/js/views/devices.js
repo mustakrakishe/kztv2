2e48ee25e6a339e0f8955937a1ce72fb39dcda52
mustakrakishe <mustakrkish@gmail.com>
2021-10-27 08:37:40 +0300

    devices.js: created showProperties(); Binded click event handler on the device table row.

--- public/js/views/devices.js
+++ public/js/views/devices.js
@@ -3,10 +3,12 @@ import Form from "../components/form.js";
 const SEARCH_INPUT = 'input#search-input';
 const SEARCH_FORM = 'form#search-form';
 const DEVICE_TABLE_CONTAINER = '#device-table-container';
-const PAGINATION_LINKS = 'a.page-link';
+const PAGINATION_LINK = 'a.page-link';
+const DEVICE_ROW = 'tr[name=device]';
 
 $(document).on('input', SEARCH_INPUT, searchDeviceHandler);
-$(document).on('click', PAGINATION_LINKS, switchPaginationPage);
+$(document).on('click', PAGINATION_LINK, switchPaginationPage);
+$(document).on('click', DEVICE_ROW, showProperties);
 
 async function searchDeviceHandler(event){
     event.preventDefault();
@@ -31,4 +33,11 @@ async function switchPaginationPage(event){
         let resultDeviceTable = response.view;
         $(DEVICE_TABLE_CONTAINER).html(resultDeviceTable);
     }
+}
+
+async function showProperties(event){
+    event.preventDefault();
+
+    let deviecRow = event.currentTarget;
+    console.log(deviecRow);
 }
