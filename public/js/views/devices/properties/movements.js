72da60544713875080f238a70da7c8617ad810ed
mustakrakishe <mustakrkish@gmail.com>
2021-10-29 14:17:02 +0300

    Cancel edit movement works.

--- public/js/views/devices/properties/movements.js
+++ public/js/views/devices/properties/movements.js
@@ -1,20 +1,33 @@
 import Form from "../../../components/form.js";
 
 const MOVEMENT_BUTTON_DELETE = 'button[name=delete-movement]';
+const MOVEMENT_FORM_UPDATE = 'form[name=update-movement]';
 const MOVEMENT_FORM_DELETE = 'form[name=delete-movement]';
 
+const MOVEMENT_ROW_INPUT = 'tr[name=movement] input';
+
 const MOVEMENT_ACTIONS_INIT = 'div[name=init]';
+const MOVEMENT_ACTIONS_EDIT = 'div[name=edit]';
 const MOVEMENT_ACTIONS_CONFIRM_DELETE = 'div[name=confirm-delete]';
 
 $(document).on('click', MOVEMENT_BUTTON_DELETE, requireDeleteConfirmation);
+$(document).on('reset', MOVEMENT_FORM_UPDATE, cancelEditMovement);
 $(document).on('submit', MOVEMENT_FORM_DELETE, confirmDelete);
 $(document).on('reset', MOVEMENT_FORM_DELETE, cancelDeleteConfirmation);
+$(document).on('input', MOVEMENT_ROW_INPUT, showEditModeActions);
+
+function cancelEditMovement(event){
+    let tableRow = $(event.target).closest('tr');
+
+    $(tableRow).find(MOVEMENT_ACTIONS_EDIT).attr('hidden', 'hidden');
+    $(tableRow).find(MOVEMENT_ACTIONS_INIT).removeAttr('hidden');
+}
 
 function requireDeleteConfirmation(){
     let actionCell = $(this).closest('td');
 
     $(actionCell).find(MOVEMENT_ACTIONS_INIT).attr('hidden', 'hidden');
-    $(actionCell).find(MOVEMENT_ACTIONS_CONFIRM_DELETE).removeAttr('hidden')
+    $(actionCell).find(MOVEMENT_ACTIONS_CONFIRM_DELETE).removeAttr('hidden');
 }
 
 function cancelDeleteConfirmation(){
@@ -35,4 +48,11 @@ async function confirmDelete(event){
         let currentTableRow = $(form).closest('tr');
         $(currentTableRow).remove();
     }
+}
+
+function showEditModeActions(event){
+    let tableRow = $(event.target).closest('tr');
+
+    $(tableRow).find(MOVEMENT_ACTIONS_INIT).attr('hidden', 'hidden');
+    $(tableRow).find(MOVEMENT_ACTIONS_EDIT).removeAttr('hidden');
 }
--- resources/views/components/devices/properties/modal/content/movements/table/row.blade.php
+++ resources/views/components/devices/properties/modal/content/movements/table/row.blade.php
@@ -2,13 +2,13 @@
 
 <tr {{ $attributes->merge(['style' => 'cursor: pointer;']) }}>
     <td class="align-bottom">
-        <input type="datetime-local" name="date" class="form-control" value="{{ str_replace(' ', 'T', $movement->date) }}">
+        <input type="datetime-local" name="date" class="form-control" form="update-movement-{{ $movement->id }}" value="{{ str_replace(' ', 'T', $movement->date) }}">
     </td>
     <td class="align-bottom">
-        <input type="text" name="location" class="form-control" value="{{ $movement->location }}">
+        <input type="text" name="location" class="form-control" form="update-movement-{{ $movement->id }}" value="{{ $movement->location }}">
     </td>
     <td class="align-bottom">
-        <input type="text" name="comment" class="form-control" value="{{ $movement->comment }}">
+        <input type="text" name="comment" class="form-control" form="update-movement-{{ $movement->id }}" value="{{ $movement->comment }}">
     </td>
 
     <td name="actions">
@@ -39,10 +39,21 @@
                 </div>
             </form>
         </div>
+        
+        <div name="edit" hidden="hidden">
+            <form id="update-movement-{{ $movement->id }}" name="update-movement" name="delete-movement" action="{{ route('devices.movements.destroy', ['device' => $movement->device_id, 'movement' => $movement->id]) }}" method="post">
+                @csrf
+                @method('delete')
+                <div class="row mx-0">
+                    <x-button class="col" style="width: 50px;">
+                        <i class="fas fa-check"></i>
+                    </x-button>
+
+                    <x-button type="reset" class="btn-secondary col ml-2" style="width: 50px;">
+                        <i class="fas fa-ban"></i>
+                    </x-button>
+                </div>
+            </form>
+        </div>
     </td>
 </tr>
-{{--
-    <x-button type="button" class="delete-movement btn-danger" style="width: 50px;">
-        <i class="fas fa-trash-alt"></i>
-    </x-button>
---}}
