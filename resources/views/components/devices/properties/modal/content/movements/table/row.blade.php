28458683ee3e7873ea61dbbcb9e74ea1fa18d7a3
mustakrakishe <mustakrkish@gmail.com>
2021-10-29 08:10:04 +0300

    Created movements table row action buttons.

--- resources/views/components/devices/properties/modal/content/movements/table/row.blade.php
+++ resources/views/components/devices/properties/modal/content/movements/table/row.blade.php
@@ -10,4 +10,14 @@
     <td>
         <input type="text" name="comment" class="form-control" value="{{ $movement->comment }}">
     </td>
+    <td>
+        <x-button class="btn-secondary" style="width: 50px;">
+            <i class="fas fa-pen"></i>
+        </x-button>
+    </td>
+    <td class="pl-0">
+        <x-button class="btn-danger" style="width: 50px;">
+            <i class="fas fa-trash-alt"></i>
+        </x-button>
+    </td>
 </tr>
