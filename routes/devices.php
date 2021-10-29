cdd789e8ca87c49a8189df1c534425b998f31844
mustakrakishe <mustakrkish@gmail.com>
2021-10-29 08:35:04 +0300

    Added movement resource routes. Created MovementController. Initializated MovementController->destroy() method.

--- routes/devices.php
+++ routes/devices.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DeviceController;
+use App\Http\Controllers\MovementController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/devices/search', [DeviceController::class, 'search'])
@@ -12,3 +13,5 @@ Route::get('/devices/fetch_data', [DeviceController::class, 'fetch_data'])
     ->name('devices.fetch_data');
     
 Route::resource('devices', DeviceController::class)->middleware('auth');
+
+Route::resource('movements', MovementController::class)->middleware('auth');
