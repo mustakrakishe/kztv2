9717aac295354f7c4e7596f3b25d9c40f52002ea
mustakrakishe <mustakrkish@gmail.com>
2021-11-02 12:38:44 +0200

    Seted fixed width for device movement properies table actions column.

--- resources/views/components/devices/properties/modal/content/movements/table/row.blade.php
+++ resources/views/components/devices/properties/modal/content/movements/table/row.blade.php
@@ -9,13 +9,9 @@
         <input type="text" name="comment" class="form-control" form="update-movement-{{ $movement->id }}" value="{{ $movement->comment }}">
     </td>
 
-    <td name="actions">
+    <td name="actions" width="124">
         <div name="init" class="row mx-0">
-            <x-button class="btn-secondary col" style="width: 50px;">
-                <i class="fas fa-pen"></i>
-            </x-button>
-            
-            <x-button name="delete-movement" class="btn-danger col ms-2" style="width: 50px;">
+            <x-button name="delete-movement" class="btn-danger col-6 ms-auto" style="width: 50px;">
                 <i class="fas fa-trash-alt"></i>
             </x-button>
         </div>
@@ -26,12 +22,12 @@
             <form name="delete-movement" action="{{ route('devices.movements.destroy', ['device' => $movement->device_id, 'movement' => $movement->id]) }}" method="post">
                 @csrf
                 @method('delete')
-                <div class="row mx-0">
-                    <x-button class="btn-danger col" style="width: 50px;">
+                <div class="d-flex">
+                    <x-button class="btn-danger me-2" style="width: 50px;">
                         <i class="fas fa-trash-alt"></i>
                     </x-button>
 
-                    <x-button type="reset" class="btn-secondary col ms-2" style="width: 50px;">
+                    <x-button type="reset" class="btn-secondary" style="width: 50px;">
                         <i class="fas fa-ban"></i>
                     </x-button>
                 </div>
@@ -42,12 +38,12 @@
             <form id="update-movement-{{ $movement->id }}" name="update-movement" action="{{ route('devices.movements.update', ['device' => $movement->device_id, 'movement' => $movement->id]) }}" method="post">
                 @csrf
                 @method('put')
-                <div class="row mx-0">
-                    <x-button class="col" style="width: 50px;">
+                <div class="d-flex">
+                    <x-button class="me-2" style="width: 50px;">
                         <i class="fas fa-check"></i>
                     </x-button>
 
-                    <x-button type="reset" class="btn-secondary col ml-2" style="width: 50px;">
+                    <x-button type="reset" class="btn-secondary" style="width: 50px;">
                         <i class="fas fa-ban"></i>
                     </x-button>
                 </div>
