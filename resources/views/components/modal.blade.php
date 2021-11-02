7e1bd2728c8a83f7b7985472b2a4acae2fdee093
mustakrakishe <mustakrkish@gmail.com>
2021-11-02 10:16:29 +0200

    modal component close button is corrected.

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -5,9 +5,7 @@
 
             <div class="modal-header">
                 {{ $title }}
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
 
             <div class="modal-body">
