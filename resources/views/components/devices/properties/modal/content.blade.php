38d3f94e7f5727bd4bb8affa4a1435995b06d20a
mustakrakishe <mustakrkish@gmail.com>
2021-11-02 10:34:28 +0200

    device properies tabs works.

--- resources/views/components/devices/properties/modal/content.blade.php
+++ resources/views/components/devices/properties/modal/content.blade.php
@@ -1,29 +1,29 @@
 <ul class="nav nav-tabs" role="tablist">
     <li class="nav-item" role="presentation">
-        <a class="nav-link active" data-toggle="tab" href="#general" role="tab" aria-controls="general" aria-selected="true">{{ __('General') }}</a>
+        <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#general" role="tab" aria-controls="general" aria-selected="true">{{ __('General') }}</button>
     </li>
     <li class="nav-item" role="presentation">
-        <a class="nav-link" data-toggle="tab" href="#movements" role="tab" aria-controls="movements">{{ __('Movements') }}</a>
+        <button class="nav-link" data-bs-toggle="tab" data-bs-target="#movements" role="tab" aria-controls="movements" aria-selected="false">{{ __('Movements') }}</button>
     </li>
     <li class="nav-item" role="presentation">
-        <a class="nav-link" data-toggle="tab" href="#hardware" role="tab" aria-controls="hardware">{{ __('Hardware') }}</a>
+        <button class="nav-link" data-bs-toggle="tab" data-bs-target="#hardware" role="tab" aria-controls="hardware" aria-selected="false">{{ __('Hardware') }}</button>
     </li>
     <li class="nav-item" role="presentation">
-        <a class="nav-link" data-toggle="tab" href="#software" role="tab" aria-controls="software">{{ __('Software') }}</a>
+        <button class="nav-link" data-bs-toggle="tab" data-bs-target="#software" role="tab" aria-controls="software" aria-selected="false">{{ __('Software') }}</button>
     </li>
     <li class="nav-item" role="presentation">
-        <a class="nav-link" data-toggle="tab" href="#repairs" role="tab" aria-controls="repairs">{{ __('Repairs') }}</a>
+        <button class="nav-link" data-bs-toggle="tab" data-bs-target="#repairs" role="tab" aria-controls="repairs" aria-selected="false">{{ __('Repairs') }}</button>
     </li>
 </ul>
 
 <div class="tab-content">
-    <div class="tab-pane fade show active" id="general" role="tabpanel">
+    <div class="tab-pane fade show active" id="general" role="tabpanel" aria-labelledby="general-tab">
         <x-devices.properties.modal.content.general :device="$device" :types="$types" />
     </div>
-    <div class="tab-pane fade" id="movements" role="tabpanel">
+    <div class="tab-pane fade" id="movements" role="tabpanel" aria-labelledby="movements-tab">
         <x-devices.properties.modal.content.movements :movements="$device->movements" />
     </div>
-    <div class="tab-pane fade" id="hardware" role="tabpanel">...</div>
-    <div class="tab-pane fade" id="software" role="tabpanel">...</div>
-    <div class="tab-pane fade" id="repairs" role="tabpanel">...</div>
+    <div class="tab-pane fade" id="hardware" role="tabpanel" aria-labelledby="hardware-tab">...</div>
+    <div class="tab-pane fade" id="software" role="tabpanel" aria-labelledby="software-tab">...</div>
+    <div class="tab-pane fade" id="repairs" role="tabpanel" aria-labelledby="repairs-tab">...</div>
 </div>
