214e2eefc5a9ca12a8581c26a5dcc3d3caba1526
mustakrakishe <mustakrkish@gmail.com>
2021-11-02 09:41:19 +0200

    Register card paddings and text aligment are corrected.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,31 +15,31 @@
                     <form id="register-form" method="POST" action="{{ route('register') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
                                 <input type="text" name="name" id="name" class="form-control" autocomplete="name" autofocus>
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
                                 <input type="password" name="password" id="password" class="form-control">
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
                                 <input type="password" name="password_confirmation" id="password-confirm" class="form-control">
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <x-button>
                                     {{ __('Register') }}
