53103d318b3df005d850e858355075dede6cf292
mustakrakishe <mustakrkish@gmail.com>
2021-11-02 13:06:45 +0200

    device properties modal full-screen in case window short width.

--- resources/views/components/devices/properties/modal.blade.php
+++ resources/views/components/devices/properties/modal.blade.php
@@ -1,3 +1,3 @@
-<x-modal id="device-properties-modal" class="modal-lg">
+<x-modal id="device-properties-modal" class="modal-lg modal-fullscreen-lg-down">
     <x-slot name="title">{{ __('Properties') }}</x-slot>
 </x-modal>
--- resources/views/components/devices/properties/modal/content/movements.blade.php
+++ resources/views/components/devices/properties/modal/content/movements.blade.php
@@ -1,4 +1,10 @@
 @props(['movements'])
 
+<div class="d-flex mt-3">
+    <x-button class="w-auto">
+        <i class="fas fa-dolly me-2"></i>
+        {{ _('Replace') }}
+    </x-button>
+</div>
 
 <x-devices.properties.modal.content.movements.table :movements="$movements"/>
