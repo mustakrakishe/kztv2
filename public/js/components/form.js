e17b82472419997ab948f7930593829d37385bc9
mustakrakishe <mustakrkish@gmail.com>
2021-11-15 16:15:56 +0200

    invalid-feedback is represented with divs instead of a list.

--- public/js/components/form.js
+++ public/js/components/form.js
@@ -48,17 +48,15 @@ class Form{
         $(form).find('.is-invalid').removeClass('is-invalid');
 
         $.each(errors, (fieldName, fieldErrors) => {
-            let ul = $.parseHTML('<ul class="invalid-feedback d-block pl-3" role="alert"></ul>');
-            let li = $.parseHTML('<strong class="small" style="display: list-item"></strong>');
+            let invalidFeedback = $.parseHTML('<div class="invalid-feedback d-block pl-3" role="alert"></div>');
 
             fieldErrors.forEach(fieldError => {
-                $(li).html(fieldError);
-                $(ul).append(li);
+                $(invalidFeedback).append('<div>' + fieldError + '</div>');
             });
 
             let input = $(form).find('[name=' + fieldName + ']');
             $(input).addClass('is-invalid');
-            $(input).after(ul);
+            $(input).after(invalidFeedback);
         });
     }
 
