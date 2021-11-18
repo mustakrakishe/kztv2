73b272f9722431122d9605e5a62c4ae7ede94ba8
mustakrakishe <mustakrkish@gmail.com>
2021-11-18 15:49:58 +0200

    deleted alter_software_table migration.

--- database/migrations/2021_10_11_110322_create_software_table.php
+++ database/migrations/2021_10_11_110322_create_software_table.php
@@ -17,6 +17,7 @@ class CreateSoftwareTable extends Migration
             $table->id();
             $table->timestamp('date')->useCurrent();
             $table->text('description')->nullable();
+            $table->text('comment')->nullable();
             $table->foreignId('device_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
         });
     }
