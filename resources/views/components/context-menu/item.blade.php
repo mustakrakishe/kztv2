32203d0acf039845eb5dc137139b194e0950ebe7
mustakrakishe <mustakrkish@gmail.com>
2021-11-23 13:29:13 +0200

    context menu submenu title eloquent highliting.

--- resources/views/components/context-menu/item.blade.php
+++ resources/views/components/context-menu/item.blade.php
@@ -1,11 +1,18 @@
-@if($attributes->has('submenu'))
+@isset($submenu)
 <li>
-    <div {{ $attributes->merge(['class' => 'btn-group dropend p-0 dropdown-item']) }}>
-        <button type="button" class="dropdown-toggle dropdown-item list-group-item-light list-group-item-action" data-bs-toggle="dropdown" aria-expanded="false">
-            {{ $title }}
+    <div {{ $attributes->merge(['class' => 'btn-group dropend p-0 dropdown-item list-group-item-light list-group-item-action']) }}>
+        <button
+            type="button"
+            class="dropdown-toggle dropdown-item list-group-item-light list-group-item-action bg-transparent"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            onmouseover="new bootstrap.Dropdown(this).show()"
+            onmouseout="new bootstrap.Dropdown(this).hide()"
+        >
+            {{ $slot ?? $title }}
         </button>
 
-        <ul class="dropdown-menu">    
+        <ul class="dropdown-menu shadow">    
             {{ $submenu }}
         </ul>
     </div>
