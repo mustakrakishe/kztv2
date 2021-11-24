135fa23c66a76a0f2024b2473f0352eb2f531b8c
mustakrakishe <mustakrkish@gmail.com>
2021-11-24 16:36:42 +0200

    move device dialog localization, added cancel button.

--- resources/views/components/device-accounting/devices/index/context-menu.blade.php
+++ resources/views/components/device-accounting/devices/index/context-menu.blade.php
@@ -1,16 +1,12 @@
 <x-context-menu id="contextmenu">
     <x-context-menu.item>
-        {{ __('Event') }}
+        {{ __('Special actions') }}
 
         <x-slot name="submenu">
-            <x-context-menu.item name="movement" href="">{{ __('Movement') }}</x-context-menu.item>
-            <x-context-menu.item name="hardware">{{ __('Hardware') }}</x-context-menu.item>
-            <x-context-menu.item name="software">{{ __('Software') }}</x-context-menu.item>
-            <x-context-menu.item name="repair">{{ __('Repair') }}</x-context-menu.item>
+            <x-context-menu.item name="move" href="{{ route('devices.movements.create', ['device' => '#']) }}">{{ __('Move') }}</x-context-menu.item>
         </x-slot>
     </x-context-menu.item>
 
-    <x-context-menu.item name="move" href="{{ route('devices.movements.create', ['device' => '#']) }}">{{ __('Move') }}</x-context-menu.item>
-    <x-context-menu.item name="edit" href="{{ route('device-accounts.edit', ['device_account' => '#']) }}">{{ __('actions.edit') }}</x-context-menu.item>
-    <x-context-menu.item name="delete" href="{{ route('devices.destroy', ['device' => '#']) }}">{{ __('actions.delete') }}</x-context-menu.item>
+    <x-context-menu.item name="edit" href="{{ route('device-accounts.edit', ['device_account' => '#']) }}">{{ __('Edit') }}</x-context-menu.item>
+    <x-context-menu.item name="delete" href="{{ route('devices.destroy', ['device' => '#']) }}">{{ __('Delete') }}</x-context-menu.item>
 </x-context-menu>
--- resources/views/components/device-accounting/movements/create.blade.php
+++ resources/views/components/device-accounting/movements/create.blade.php
@@ -2,6 +2,7 @@
     <x-slot name="title">{{ __('Move') }}</x-slot>
     <x-device-accounting.movements.create.form :statuses="$statuses" :movement="$device->last_movement" />
     <x-slot name="footer">
-        <x-button form="create-movement-form">{{ __('dialog.actions.apply') }}</x-button>
+        <x-button class="btn-secondary" data-bs-dismiss="modal">{{ __('dialog.actions.cancel') }}</x-button>
+        <x-button form="create-movement-form">{{ __('dialog.actions.ok') }}</x-button>
     </x-slot>
 </x-modal>
